bbde6228acf08a4325e91336a5f1d18e9426289c
Cristian Cabrera <cristiancabrera@Cristians-iMac-3.home>
2023-03-25 23:05:12 -0700

    addItemToDOM() & addItemToStorage with Notes UPDATED

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ function addItemToDOM(item){
   itemList.appendChild(li);
 }
 
+// Local storage
 function addItemToStorage(item){
   let itemsFromStorage
 
@@ -54,6 +55,7 @@ function addItemToStorage(item){
     itemsFromStorage = []
   } else {
     itemsFromStorage = JSON.parse(localStorage.getItem('items'))
+    // JSON.parse(localStorage.getItem('items')) is used to retrieve the array of items from localStorage (which has been stored as a JSON string), and convert it back into a JavaScript array.
   }
 
   // Add new item to array
@@ -61,7 +63,30 @@ function addItemToStorage(item){
 
   // Convert to JSON string and set to local storage
   localStorage.setItem('items', JSON.stringify(itemsFromStorage))
+  // localStorage.setItem is a method that is used to store a key-value pair in the local storage. The first parameter is the key, in this case, 'items', and the second parameter is the value, which is the stringified version of the itemsFromStorage array.
+
+// JSON.stringify() is a built-in method in JavaScript that converts a JavaScript object or value to a JSON string. In this case, it takes the itemsFromStorage array and converts it to a JSON string so that it can be stored in the browser's local storage.
 }
+// How addItemToStorage() works:
+// First, it checks if there is anything in local storage under the key 'items'. If there isn't, it initializes an empty array. If there is, it retrieves the array from local storage and parses it into a JavaScript array using JSON.parse().
+// Next, the new item is added to the array using push().
+// Finally, the updated array is converted to a JSON string using JSON.stringify() and set back into local storage under the key 'items' using localStorage.setItem().
+
+
+// localStorage.setItem('name', 'brad')
+// console.log(localStorage.getItem('name'))
+// // localStorage.removeItem('name')
+// localStorage.clear()
+
+// local storage example:
+// const myArray = [1, 2, 3];
+// localStorage.setItem('myKey', JSON.stringify(myArray));
+
+// This stores the array [1, 2, 3] in localStorage under the key 'myKey', but as a JSON string.
+// Later, when you want to retrieve the array from localStorage, you use localStorage.getItem('myKey') to get the JSON string value. This is where JSON.parse() comes in - it is used to convert the JSON string back into the original JavaScript object. For example:
+
+// const myArrayFromStorage = JSON.parse(localStorage.getItem('myKey'));
+// console.log(myArrayFromStorage); // outputs: [1, 2, 3]
 
 function createButton(classes) {
   const button = document.createElement('button');
@@ -138,8 +163,3 @@ itemFilter.addEventListener('input', filterItems)
 
 checkUI()
 
-
-localStorage.setItem('name', 'brad')
-console.log(localStorage.getItem('name'))
-// localStorage.removeItem('name')
-localStorage.clear()
