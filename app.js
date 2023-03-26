465f1eb92084d22f93e8dfd5e8fd46b58e8ef881
Cristian Cabrera <cristiancabrera@Cristians-iMac-3.home>
2023-03-25 22:50:22 -0700

    addItemToDOM() & addItemToStorage with Notes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear')
 const itemFilter = document.getElementById('filter')
 
-function addItem(e) {
+function onAddItemSubmit(e) {
   e.preventDefault();
 
   const newItem = itemInput.value;
@@ -15,18 +15,52 @@ function addItem(e) {
     return;
   }
 
+  // // Create list item
+  // const li = document.createElement('li');
+  // li.appendChild(document.createTextNode(newItem));
+
+  // const button = createButton('remove-item btn-link text-red');
+  // li.appendChild(button);
+
+  // // Add li to DOM  
+  // itemList.appendChild(li);
+
+  // Create item DOM element
+  addItemToDOM(newItem)
+
+  // Add item to local storage
+  addItemToStorage(newItem)
+
+  checkUI()
+  itemInput.value = '';
+}
+
+function addItemToDOM(item){
   // Create list item
   const li = document.createElement('li');
-  li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(item));
 
   const button = createButton('remove-item btn-link text-red');
   li.appendChild(button);
 
   // Add li to DOM  
   itemList.appendChild(li);
+}
 
-  checkUI()
-  itemInput.value = '';
+function addItemToStorage(item){
+  let itemsFromStorage
+
+  if(localStorage.getItem('items') === null){
+    itemsFromStorage = []
+  } else {
+    itemsFromStorage = JSON.parse(localStorage.getItem('items'))
+  }
+
+  // Add new item to array
+  itemsFromStorage.push(item)
+
+  // Convert to JSON string and set to local storage
+  localStorage.setItem('items', JSON.stringify(itemsFromStorage))
 }
 
 function createButton(classes) {
@@ -97,7 +131,7 @@ function checkUI(){
 
 
 // Event Listeners
-itemForm.addEventListener('submit', addItem);
+itemForm.addEventListener('submit', onAddItemSubmit);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
 itemFilter.addEventListener('input', filterItems)
@@ -105,3 +139,7 @@ itemFilter.addEventListener('input', filterItems)
 checkUI()
 
 
+localStorage.setItem('name', 'brad')
+console.log(localStorage.getItem('name'))
+// localStorage.removeItem('name')
+localStorage.clear()
